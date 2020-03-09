9201fe26a92f42e6e724f741d454c30e5db47a69
Denny <203371@NTTDATA.COM>
2020-03-09 12:17:22 -0700

    Added more images to tag all script

--- openshift/tagging/tag-all.sh
+++ openshift/tagging/tag-all.sh
@@ -2,7 +2,7 @@
 oc project c2mvws-tools
 oc apply -f tagging-pipeline.yaml
 
-array=('educ-codetable-api-master' 'educ-digitalid-api-master' 'educ-pen-demog-api-master' 'educ-pen-request-backend' 'educ-pen-request' 'educ-pen-request-api-master' 'educ-pen-request-frontend-static' 'educ-services-card-api-master' 'educ-soam-api-master' 'educ-student-admin-backend' 'educ-student-admin-frontend-static' 'educ-student-api-master')
+array=('educ-codetable-api-master' 'educ-pen-request-email-api-master' 'sso' 'educ-digitalid-api-master' 'educ-pen-demog-api-master' 'educ-pen-request-backend' 'educ-pen-request' 'educ-pen-request-api-master' 'educ-pen-request-frontend-static' 'educ-services-card-api-master' 'educ-soam-api-master' 'educ-student-admin-backend' 'educ-student-admin-frontend-static' 'educ-student-api-master')
 for item in ${array[*]}
 do
     echo "Item: $item"
