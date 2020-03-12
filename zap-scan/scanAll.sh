86b062f0e9536c0db006a5c6b41d9eb5a4725f2a
Denny <203371@NTTDATA.COM>
2020-03-12 11:27:05 -0700

    Added zapscan resources

---
+++ zap-scan/scanAll.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+oc project c2mvws-tools
+oc apply -f scan-pipeline.yaml
+
+## 'digitalid-api' 'pen-demographics-api' 'pen-request' 'pen-request-api' 'services-card-api' 'soam-api' 'student-admin' 'student-api'}
+url_array=( 'pen-request-email-api' 'pen-request-api' )
+key_array=( 'penrequest-email-api' 'penrequest-api' )
+for index in ${!url_array[*]}
+do
+    echo "Item: ${url_array[$item]}"
+    oc start-build scan-pipeline -w --env=PROJECT_NAME=${url_array[$index]} --env=${key_array[$index]}= --env=NAMESPACE='dev'
+done
+
+oc delete bc/scan--pipeline
