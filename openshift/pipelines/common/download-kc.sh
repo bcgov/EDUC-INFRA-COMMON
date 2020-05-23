b1b8c684b708e174e9abc8189c4ae95b62c0e1a7
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2020-05-22 19:54:47 -0700

    Update.

--- openshift/pipelines/common/download-kc.sh
+++ openshift/pipelines/common/download-kc.sh
@@ -1,6 +1,6 @@
 namespace=$1
 
-if [ ! -f home/jenkins/workspace/$namespace-tools/keycloak-9.0.3.zip ]
+if [ ! -f /tmp/keycloak-9.0.3.zip ]
 then
     echo 'Downloading and unzipping keycloak setup files...'
     wget -P /tmp -nc https://downloads.jboss.org/keycloak/9.0.3/keycloak-9.0.3.zip
