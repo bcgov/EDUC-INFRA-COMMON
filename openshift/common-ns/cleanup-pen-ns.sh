6b5b9e7ec0ab6b3bcc4629ab654f22f487d68429
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2020-06-02 16:43:24 -0700

    Cleanup updates.

--- openshift/common-ns/cleanup-pen-ns.sh
+++ openshift/common-ns/cleanup-pen-ns.sh
@@ -21,19 +21,19 @@ oc delete configmap pen-request-api-config-map
 oc delete configmap pen-request-api-dev-setup-config
 oc delete dc pen-request-api-master
 
-#Remove PEN Demog API
-oc delete route pen-demographics-api-master
-oc delete svc pen-demographics-api-master
-oc delete configmap pen-demog-api-config-map
-oc delete configmap pen-demographics-api-dev-setup-config
-oc delete dc pen-demographics-api-master
-
 #Remove Services Card API
 oc delete route services-card-api-master
 oc delete svc services-card-api-master
 oc delete configmap services-card-api-config-map
 oc delete configmap services-card-api-dev-setup-config
 oc delete dc services-card-api-master
+
+#Remove PEN Demog API
+oc delete route pen-demographics-api-master
+oc delete svc pen-demographics-api-master
+oc delete configmap pen-demog-api-config-map
+oc delete configmap pen-demographics-api-dev-setup-config
+oc delete dc pen-demographics-api-master
 
 #Remove SOAM API
 oc delete route soam-api-master
@@ -42,9 +42,10 @@ oc delete configmap soam-api-config-map
 oc delete dc soam-api-master
 
 #Remove SOAM SSO
-oc delete all,rc,svc,dc,route,pvc,secret,sa -l app-name=sso
+oc delete all,rc,svc,dc,route,pvc,secret,configmap,sa -l app-name=sso
 oc delete pvc -l statefulset=sso-pgsql-dev
 oc delete cm -l cluster-name=sso-pgsql-dev
+oc delete configmap soam-sso-config-map
 
 #Remove NATS
 oc delete all,rc,svc,dc,route,pvc,secret -l app=nats
