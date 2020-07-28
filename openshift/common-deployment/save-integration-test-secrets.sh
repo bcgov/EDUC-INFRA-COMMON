db6aebcd04a40a472340ea21d36b73686b24f1fe
Mingwei <mingwei.y@gmail.com>
2020-07-28 12:07:17 -0700

    Fix the brackets

--- openshift/common-deployment/save-integration-test-secrets.sh
+++ openshift/common-deployment/save-integration-test-secrets.sh
@@ -5,6 +5,7 @@ OWNER=$4
 REPO_NAME=$5
 TARGET_ENVIRONMENT=$6
 APP_NAME=$7
+CLIENT_ID=$8
 
 SOAM_KC_REALM_ID="master"
 KCADM_FILE_BIN_FOLDER="/tmp/keycloak-9.0.3/bin"
@@ -19,15 +20,15 @@ SOAM_KC=$OPENSHIFT_NAMESPACE-$envValue.pathfinder.gov.bc.ca
 
 $KCADM_FILE_BIN_FOLDER/kcadm.sh config credentials --server https://$SOAM_KC/auth --realm $SOAM_KC_REALM_ID --user "$SOAM_KC_LOAD_USER_ADMIN" --password "$SOAM_KC_LOAD_USER_PASS"
 
-getPenRequestItClientID(){
-    executorID= $KCADM_FILE_BIN_FOLDER/kcadm.sh get clients -r $SOAM_KC_REALM_ID --fields 'id,clientId' | grep -B2 '"clientId" : "pen-request-api-it"' | grep -Po "(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}"
+getItClientID(){
+    executorID= $KCADM_FILE_BIN_FOLDER/kcadm.sh get clients -r $SOAM_KC_REALM_ID --fields 'id,clientId' | grep -B2 "\"clientId\" : \"${CLIENT_ID}\"" | grep -Po "(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}"
 }
-penRequestItClientID=$(getPenRequestItClientID)
+itClientID=$(getItClientID)
 
-getPenRequestItClientSecret(){
-    executorID= $KCADM_FILE_BIN_FOLDER/kcadm.sh get clients/$penRequestItClientID/client-secret -r $SOAM_KC_REALM_ID | grep -Po "(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}"
+getItClientSecret(){
+    executorID= $KCADM_FILE_BIN_FOLDER/kcadm.sh get clients/$itClientID/client-secret -r $SOAM_KC_REALM_ID | grep -Po "(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}"
 }
-penRequestItClientSecret=$(getPenRequestItClientSecret)
+itClientSecret=$(getItClientSecret)
 
 
 if [ ! -f /tmp/github-tools-v1.0.0-linux-x64.tar.gz ]
@@ -40,5 +41,5 @@ fi
 
 /tmp/github-tools/bin/github-tools createSecret ${TOKEN} ${OWNER} ${REPO_NAME} SOAM_DISCOVERY_URL https://${TARGET_ENVIRONMENT}.pathfinder.gov.bc.ca/auth/realms/master/.well-known/openid-configuration
 /tmp/github-tools/bin/github-tools createSecret ${TOKEN} ${OWNER} ${REPO_NAME} SOAM_CLIENT_ID ${APP_NAME}-it
-/tmp/github-tools/bin/github-tools createSecret ${TOKEN} ${OWNER} ${REPO_NAME} SOAM_CLIENT_SECRET ${penRequestItClientSecret}
+/tmp/github-tools/bin/github-tools createSecret ${TOKEN} ${OWNER} ${REPO_NAME} SOAM_CLIENT_SECRET ${itClientSecret}
 /tmp/github-tools/bin/github-tools createSecret ${TOKEN} ${OWNER} ${REPO_NAME} API_URL https://${APP_NAME}-${TARGET_ENVIRONMENT}.pathfinder.gov.bc.ca
